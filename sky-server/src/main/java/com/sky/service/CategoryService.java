b28413b2334ee2e5ae2704328c518ffc8f8589d8
YGB <517420999@qq.com>
2025-01-10 19:11:42 +0800

    新增分类

--- sky-server/src/main/java/com/sky/service/CategoryService.java
+++ sky-server/src/main/java/com/sky/service/CategoryService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.result.PageResult;
 import org.springframework.stereotype.Service;
 
 public interface CategoryService {
@@ -8,4 +10,11 @@ public interface CategoryService {
      * 新增分类
      */
     void save(CategoryDTO categoryDTO);
+
+    /**
+     * 分类分页查询
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    PageResult page(CategoryPageQueryDTO categoryPageQueryDTO);
 }
