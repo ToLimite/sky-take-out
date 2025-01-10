258e80a6dca097a586b242473f2b8db7a5a5e041
YGB <517420999@qq.com>
2025-01-10 11:17:47 +0800

    细节修正（log、注释）

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -25,4 +25,12 @@ public interface EmployeeService {
      * @param id
      */
     void startOrStop(Integer status, Long id);
+
+    /**
+     * 根据id查询员工信息
+     * @return
+     */
+    Employee getById(Long id);
+
+    void update(EmployeeDTO employeeDTO);
 }
