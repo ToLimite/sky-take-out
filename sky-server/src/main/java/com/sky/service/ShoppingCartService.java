465c2018724cfa409637c46a0919bbd838edd849
YGB <517420999@qq.com>
2025-01-24 14:32:21 +0800

    删除购物车中一个物品

--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -12,9 +12,21 @@ public interface ShoppingCartService {
      */
     void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
 
-
+    /**
+     * 查看购物车
+     * @return
+     */
     List<ShoppingCart> showShoppingCart();
 
+    /**
+     * 清空购物车
+     */
     void cleanShoppingCart();
+
+    /**
+     * 删除购物车中一个商品
+     * @param shoppingCartDTO
+     */
+    void subShoppingCart(ShoppingCartDTO shoppingCartDTO);
 
 }
